052b5b6f0a182b83fd88818b6e815a7080605686
Conifer <64596327+conifer-dev@users.noreply.github.com>
2022-02-09 07:41:53 +0000

    Update SpriteAnimator.swift
    Fixed flipping image, still requires work to fix a bug when moving and the sprite is flipped.

--- Sources/Aeni/SpriteAnimator.swift
+++ Sources/Aeni/SpriteAnimator.swift
@@ -14,6 +14,7 @@ public final class SpriteAnimator {
 
     internal var timeSinceStart: Float32 = 0
     internal var isAnimationFinished: Bool = false
+    internal var spriteSize: Vector2
 
     public init(sprite: Sprite, origin: Vector2, rotation: Float32, startingFrame: UInt, endingFrame: UInt, column: UInt, duration: Float32, animationSpeed: Float32, repeatable: Bool, tintColor: Color) {
         self.sprite = sprite
@@ -26,21 +27,24 @@ public final class SpriteAnimator {
         self.animationSpeed = animationSpeed
         self.repeatable = repeatable
         self.tintColor = tintColor
+
+        self.spriteSize = sprite.frameDimensions
     }
 }
 
 extension SpriteAnimator {
     public func render() {
-        Raylib.drawTexturePro(self.sprite.spriteSheet, Rectangle(x: Float32(startingFrame) * Float32(self.sprite.frameDimensions.x), y: Float32(column) * Float32(self.sprite.frameDimensions.y), width: Float32(self.sprite.frameDimensions.x), height: Float32(self.sprite.frameDimensions.y)), Rectangle(x: self.sprite.position.x, y: self.sprite.position.y, width: Float32(self.sprite.frameDimensions.x) * self.sprite.scale.x, height: self.sprite.frameDimensions.y * self.sprite.scale.x), Vector2(x: self.origin.x, y: self.origin.y), self.rotation, self.tintColor)
+        Raylib.drawTexturePro(self.sprite.spriteSheet, Rectangle(x: Float32(startingFrame) * Float32(self.sprite.frameDimensions.x), y: Float32(column) * Float32(self.sprite.frameDimensions.y), width: Float32(self.spriteSize.x), height: Float32(self.spriteSize.y)), Rectangle(x: self.sprite.position.x, y: self.sprite.position.y, width: Float32(self.sprite.frameDimensions.x) * self.sprite.scale.x, height: self.sprite.frameDimensions.y * self.sprite.scale.x), Vector2(x: self.origin.x, y: self.origin.y), self.rotation, self.tintColor)
     }
 }
 
 extension SpriteAnimator {
     public func update() {
 
-        self.play()
-
         if !isAnimationFinished {
+            timeSinceStart += Raylib.getFrameTime()
+            duration -= Raylib.getFrameTime()
+
             if timeSinceStart >= self.animationSpeed {
                 timeSinceStart = 0
                 startingFrame += 1
@@ -59,10 +63,15 @@ extension SpriteAnimator {
 }
 
 extension SpriteAnimator {
-    public func play() {
-        if !isAnimationFinished {
-            timeSinceStart += Raylib.getFrameTime()
-            duration -= Raylib.getFrameTime()
+    public func flipSprite(horizontal: Bool, vertical: Bool) {
+
+        if horizontal && vertical {
+            self.spriteSize.x *= -1
+            self.spriteSize.y *= -1
+        } else if horizontal {
+            self.spriteSize.x *= -1
+        } else if vertical {
+            self.spriteSize.y *= -1
         }
     }
 }
