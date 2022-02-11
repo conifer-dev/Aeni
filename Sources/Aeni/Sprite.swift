ec8674091c6d7d400888e8f172c49f8faa9a6583
Conifer <64596327+conifer-dev@users.noreply.github.com>
2022-02-11 09:20:09 +0000

    Sprite position is no longer in Sprite types ownership
    Ownership of position is moved to the Sprite Animator. An obvious failure to spot the issue from my end.
    
    Animations now control their own positioning in the world.
    
    destRect is only accessible as a getter. Setter has been set to private as the user has no reason to be modifying the internal destRect.

--- Sources/Aeni/Sprite.swift
+++ Sources/Aeni/Sprite.swift
@@ -7,15 +7,13 @@ public final class Sprite {
     public var spriteSheet: Texture2D
     public var frameDimensions: Vector2
     public var scale: Vector2
-    public var position: Vector2
 
     // Internal variable decleration.
     internal var sourceRect: Rectangle = Rectangle()
 
-    public init(spriteSheet: Texture2D, frameDimensions: Vector2, scale: Vector2, position: Vector2) {
+    public init(spriteSheet: Texture2D, frameDimensions: Vector2, scale: Vector2) {
         self.spriteSheet = spriteSheet
         self.frameDimensions = frameDimensions
         self.scale = scale
-        self.position = position
     }
 }
